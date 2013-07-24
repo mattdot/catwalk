2f044d5540029736212e1087e39ed453bd37ba01
Matt Dotson <matt.dotson@gmail.com>
2013-07-24 15:49:29 -0700

    updated version number

--- src/Catwalk/Properties/AssemblyInfo.cs
+++ src/Catwalk/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.2.0")]
 
-[assembly: AssemblyInformationalVersion("0.1.0-alpha")]
+[assembly: AssemblyInformationalVersion("0.1.2-alpha")]
