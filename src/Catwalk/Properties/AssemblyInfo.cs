50368011570f67f6304e35f6db11f4b1703ba057
Matt Dotson <matt.dotson@gmail.com>
2013-07-24 16:35:37 -0700

    updated version

--- src/Catwalk/Properties/AssemblyInfo.cs
+++ src/Catwalk/Properties/AssemblyInfo.cs
@@ -13,24 +13,13 @@ using System.Runtime.InteropServices;
 #else
 [assembly: AssemblyConfiguration("RELEASE")]
 #endif
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Matt Dotson")]
 [assembly: AssemblyProduct("Catwalk")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.2.0")]
-
-[assembly: AssemblyInformationalVersion("0.1.2-alpha")]
+[assembly: AssemblyInformationalVersion("0.1.2")]
